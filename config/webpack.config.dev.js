99a38f2de7d4621835e6b41a66f3185000c5b806
yidafu <tiaozuwushi@qq.com>
2018-09-15 00:10:32 +0800

    add alias: @ as /src

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -87,6 +87,7 @@ module.exports = {
       
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+      '@': paths.appSrc,
       'react-native': 'react-native-web',
     },
     plugins: [
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
-import Slider from '@/Component/Slider'
+import Slider from '@/Components/Slider'
 
 export default class Home extends Component {
   render() {
