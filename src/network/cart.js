4256a585b83dcc30effabc338b755b780daacf0b
yidafu <tiaozuwushi@qq.com>
2018-12-04 23:42:20 +0800

    request to add a present to cart

--- src/network/cart.js
+++ src/network/cart.js
@@ -3,3 +3,7 @@ import curry from '@/utils/curry'
 
 // TODO Get user_id from store
 export const all = curry(request.get, '/users/123/cart')
+
+export function addToCart(present) {
+  return (curry(request.post,'/user/123/cart', present)(present))
+}
